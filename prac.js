579e2e41d7ded43caeca9796d9dcde435893aa0a
vanessarabie <vanessarabie12.com>
2023-11-21 19:00:57 +0200

    my javascript practice

---
+++ prac.js
@@ -0,0 +1,121 @@
+// freecode camp 
+
+// first i studied on the use of comment where // are for single line and /**for mult line comment */ examples are these already
+
+//second i learnt on declaring variables such as 
+
+var myname;
+console.log(myname);
+
+// assigning values to variables
+var vari;
+vari = 9;
+console.log(vari);
+
+//initialising variables
+var me = 78;
+console.log(me);
+
+//data string variables
+
+var ab = "firstname";
+var  ac = "lastname";
+fullname = "firstname" + "lastname";
+console.log(fullname);
+
+//summation of numbers
+var aa = 2;
+var ab = 9;
+var ac = aa  + ab;
+console.log(ac);
+
+//subtration of numbers
+
+var ab = 2;
+var ac = 9;
+var ad = ab  - ac;
+console.log(ad);
+
+//multiplication of numbers
+var ae = 2;
+var ad = 9;
+var af = ae  * ad;
+console.log(af);
+
+//quotient of numbers
+
+var v = 19;
+var p = 9;
+ n = v  / p;
+console.log(n);
+
+// modulas of number
+
+var ab = 2;
+var ac = 9;
+var mod = ab  % ac;
+console.log(mod);
+
+
+var ab = 10;
+var ac = 2;
+var mod1 = ab  % ac;
+console.log(mod1);
+
+
+//still doing more..........
+
+
+
+
+
+
+// js challenge 
+
+// variable is memory space to store data
+// the variables that we use are var , let, const 
+
+//use let
+let a = 2;
+console.log(a);
+
+let num ;
+num = 5;
+console.log(num);
+
+
+let numone = 3;
+let numtwo = 3;
+console.log(numtwo);
+
+
+
+
+let numOne = 4;
+let numTwo = numOne;
+console.log(numTwo);
+
+//use const
+
+const tst = 9;
+console.log(tst);
+
+const were = 2;
+console.log(were);
+
+//use let
+const nm = 5;
+console.log(nm);
+ 
+//use let
+let m = 2;
+m = 6;
+console.log(m);
+
+// use const
+const na = 2;
+na = 6;
+console.log(na);
+
+
+
