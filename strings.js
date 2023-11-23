ded74e609ffe5269bb3f4a94373dee2ca42e068f
vanessarabie <vanessarabie12.com>
2023-11-23 15:29:40 +0200

    pushed well

---
+++ strings.js
@@ -0,0 +1,30 @@
+// strings
+
+console.log(typeof 1);
+const num = 1;
+console.log(typeof num);
+
+
+let text = "hello";
+console.log(text);
+text = "hii";
+console.log(text);
+text = "mama";
+console.log(text);
+
+const numone = "hello";
+const numtwo = "world";
+combined = numone + ' ' + numtwo;
+console.log(combined);
+
+const mari = "text";
+console.log(typeof mari);
+
+const txt = 'hell world';
+console.log('the value of text is: ' + txt);
+
+const mno = "hello";
+const nmo = 'mari';
+const omn = `am here`;
+console.log(mno, nmo, omn);
+
