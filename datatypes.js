05fdf24e176a65196bab99fdf5a9cb1edb0fb47e
vanessarabie <vanessarabie12.com>
2023-11-28 23:35:39 +0200

    use of typeof operator

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+ // use of typeof operator
+ 
+ let year = 1990;
+console.log(typeof year);
+
+console.log(typeof 1990);
+
+const name = 'juniour';
+console.log(typeof name);
+console.log(name);
+
+let typeOfYear = typeof year;
+console.log(typeOfYear); 
+console.log(typeof typeOfYear);
+
+// boolean 
+
+let ishappy = true;
+let isstringlong = false;
+let isgameover = false;
+continueloop = true;
+
+
+console.log(ishappy);
+console.log(typeof isstringlong);
+console.log(true);
+
+//number
+const year1 = 1991;
+let delayInSeconds = 0.00016;
+let area = (16 * 3.14);
+let halfArea = area / 2;
+
+console.log(year1); 
+console.log(typeof year1);
