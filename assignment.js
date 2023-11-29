b5f35acfbc709fe48baec9cb6db4e27cb25c6e2b
vanessarabie <vanessarabie12.com>
2023-11-30 00:07:50 +0200

    did some exercices

--- assignment.js
+++ assignment.js
@@ -25,3 +25,59 @@ let Name2 = 'Helen Richards';
 Phone = '0800 1111';
 Email = 'libero@convallis.edu'
 console.log(Name2,' ',Phone, ' ',' ',' ', Email);
+
+
+// there are many possible and correct variable names
+let rosePrice = 8;
+let lilyPrice = 10;
+let tulipPrice = 2;
+
+let numberOfRoses = 70;
+let numberOfLilies = 50;
+let numberOfTulips = 120;
+
+let rosesValue = rosePrice * numberOfRoses;
+let liliesValue = lilyPrice * numberOfLilies;
+let tulipsValue = tulipPrice * numberOfTulips;
+
+let total = rosesValue + liliesValue + tulipsValue;
+console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", value:", rosesValue);
+console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
+console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
+
+
+
+// deduction on roses and lilies by 20 and 30 respectively
+
+const rosePrice1 = 8;
+const lilyPrice1 = 10;
+const tulipPrice1 = 2;
+
+let numberOfRoses1 = 70;
+let numberOfLilies1 = 50;
+let numberOfTulips1 = 120;
+
+let rosesValue1 = rosePrice1 * numberOfRoses1;
+let liliesValue1 = lilyPrice1 * numberOfLilies1;
+let tulipsValue1 = tulipPrice1 * numberOfTulips1;
+
+let total1 = rosesValue1 + liliesValue1 + tulipsValue1;
+
+console.log("Rose – unit price:", rosePrice1, ", quantity:", numberOfRoses1, ", value:", rosesValue1);
+console.log("Lily – unit price:", lilyPrice1, ", quantity:", numberOfLilies1, ", value:", liliesValue1);
+console.log("Tulip – unit price:", tulipPrice1, ", quantity:", numberOfTulips1, ", value:", tulipsValue1);
+console.log("Total: ", total);
+
+numberOfRoses1 = numberOfRoses1 - 20;
+numberOfLilies1 = numberOfLilies1 - 30;
+
+rosesValue1 = rosePrice1 * numberOfRoses1;
+liliesValue1 = lilyPrice1 * numberOfLilies1;
+tulipsValue1 = tulipPrice1 * numberOfTulips1;
+
+total1 = rosesValue1 + liliesValue1 + tulipsValue1;
+
+console.log("Rose – unit price:", rosePrice1, ", quantity:", numberOfRoses1, ", value:", rosesValue1);
+console.log("Lily – unit price:", lilyPrice1, ", quantity:", numberOfLilies1, ", value:", liliesValue1);
+console.log("Tulip – unit price:", tulipPrice1, ", quantity:", numberOfTulips1, ", value:", tulipsValue1);
+console.log("Total: ", total1);
